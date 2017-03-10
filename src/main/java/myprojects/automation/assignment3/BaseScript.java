a94e07aa07e5359c69218f637fef2faac6eb0518
Василий Лада <vlada@web100-ck.local>
2017-03-10 17:39:41 +0200

    Initial commit

---
+++ src/main/java/myprojects/automation/assignment3/BaseScript.java
@@ -0,0 +1,44 @@
+package myprojects.automation.assignment3;
+
+import myprojects.automation.assignment3.utils.Properties;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.events.EventFiringWebDriver;
+
+import java.io.File;
+
+/**
+ * Base script functionality, can be used for all Selenium scripts.
+ */
+public abstract class BaseScript {
+
+    /**
+     *
+     * @return New instance of {@link WebDriver} object. Driver type is based on passed parameters
+     * to the automation project, returns {@link ChromeDriver} instance by default.
+     */
+    public static WebDriver getDriver() {
+        String browser = Properties.getBrowser();
+        switch (browser) {
+            // TODO prepare required WebDriver instance according to browser type
+            default:
+                System.setProperty(
+                        "webdriver.chrome.driver",
+                        new File(BaseScript.class.getResource("/chromedriver.exe").getFile()).getPath());
+                return new ChromeDriver();
+        }
+    }
+
+    /**
+     * Creates {@link WebDriver} instance with timeout and browser window configurations.
+     *
+     * @return New instance of {@link EventFiringWebDriver} object. Driver type is based on passed parameters
+     * to the automation project, returns {@link ChromeDriver} instance by default.
+     */
+    public static EventFiringWebDriver getConfiguredDriver() {
+        WebDriver driver = getDriver();
+
+       // TODO configure browser window (set timeouts, browser pindow position) and connect loggers.
+        throw new UnsupportedOperationException("Method doesn't return configured WebDriver instance");
+    }
+}
---
+++ src/main/java/myprojects/automation/assignment3/tests/CreateCategoryTest.java
@@ -0,0 +1,19 @@
+package myprojects.automation.assignment3.tests;
+
+import myprojects.automation.assignment3.BaseScript;
+
+public class CreateCategoryTest extends BaseScript {
+    public static void main(String[] args) throws InterruptedException {
+        // TODO prepare driver object
+        // WebDriver driver = getConfiguredDriver();
+        // ...
+
+        // login
+
+        // create category
+
+        // check that new category appears in Categories table
+
+        // finish script
+    }
+}
